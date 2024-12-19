9b75fa0cecb28c3324b97bdeffc9db94688c5f24
irinadeeva <deeva97@me.com>
2024-12-19 15:00:03 +0300

    Initial Commit

---
+++ Mimicry/MimicryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimicryApp.swift
+//  Mimicry
+//
+//  Created by Irina Deeva on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MimicryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
