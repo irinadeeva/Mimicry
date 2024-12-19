ee9acc137e3fc0dfc122ccd9f001af9e338381cc
irinadeeva <deeva97@me.com>
2024-12-19 19:23:45 +0300

    done

--- Mimicry/ContentView.swift
+++ Mimicry/ContentView.swift
@@ -8,17 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @Namespace private var animationNamespace
+  @State private var expand = false
+
+  var body: some View {
+    if expand {
+
+      ZStack(alignment: .center) {
+        RoundedRectangle(cornerRadius: 30.0)
+          .matchedGeometryEffect(id: "shape", in: animationNamespace)
+          .foregroundColor(Color.blue)
+          .frame(
+            width: expand ? 380 : 100,
+            height: expand ? 300 : 50
+          )
+
+        VStack(alignment: .center) {
+          HStack {
+
+            Button(action: {
+              withAnimation {
+                expand.toggle()
+              }
+            }) {
+              HStack {
+                Image(systemName: "arrowshape.backward.fill")
+                Text("Back")
+              }
+              .matchedGeometryEffect(id: "text", in: animationNamespace)
+              .font(.headline)
+              .foregroundColor(.white)
+              .bold()
+              .padding()
+            }
+            Spacer()
+          }
+          Spacer()
         }
         .padding()
+
+      }
+      .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 300)
+    } else {
+      VStack {
+        Spacer()
+
+        HStack {
+          Spacer()
+
+          Button(action: {
+            withAnimation {
+              expand.toggle()
+            }
+          }) {
+            Color.blue
+              .cornerRadius(15)
+              .matchedGeometryEffect(id: "shape", in: animationNamespace)
+              .frame(
+                width: !expand ? 100 : 380,
+                height: !expand ? 50 : 300
+              )
+              .overlay(
+                HStack {
+                  Text("Open")
+                    .padding()
+                }
+                  .font(.headline)
+                  .foregroundColor(.white)
+                  .matchedGeometryEffect(id: "text", in: animationNamespace, properties: .position, anchor: .leading)
+              )
+          }
+        }
+        .padding()
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
